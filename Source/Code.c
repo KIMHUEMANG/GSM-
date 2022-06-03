6abae28597922f7ff27d6af3045d66287d86d900
Kim Hue Mang 김희망 <105429536+KIMHUEMANG@users.noreply.github.com>
2022-06-04 01:01:46 +0900

    Update Code.c

--- Source/Code.c
+++ Source/Code.c
@@ -4,11 +4,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+int InputKey;
+
+//User의 Value
 long long gold = 11500;
 long long hp = 1000;
 long long pow = 100;
 int FirstGradePrice = 300;
+int makegold = 1;
+int makegoldLV = 1;
+long long makegoldLvPrice = 100;
 
+//학생수
 int FirstGrade = 0;
 int First2Grade = 0;
 int SecondGrade = 0;
@@ -16,20 +23,23 @@ int Second2Grade = 0;
 int ThirdGrade = 0;
 int AllOfStudent = 0;
 
+//Feature Function
 void gotoxy(int x, int y);
 void Admission();
 void GoldScreenUpdate();
+void StateUpdate();
+void shop();
+void shopfeature();
+void goldLvup();
 
 //메인화면
 void mainscreen() {
-	int AllOfStudent = FirstGrade + First2Grade + SecondGrade + Second2Grade + ThirdGrade;
-	
-	0;
+
 	gotoxy(1, 1);
 		printf("돈 : %lld 원  \n", gold);
 	gotoxy(1, 2);
-		printf("돈벌기 (s) ");
-	gotoxy(71, 1);
+		printf("돈벌기 (s)");
+	gotoxy(1, 4);
 		printf("입학받기[ - 300원 ] (a) ");
 	gotoxy(71, 3);
 		printf("총 학생 수 : %d", AllOfStudent);
@@ -46,21 +56,32 @@ void mainscreen() {
 	gotoxy(70, 15);
 		printf(" ---------------------------------------\n");
 	gotoxy(70, 16);
-		printf("|   상점가기 (1) 기능대회(2) 보스전(3)  |\n");
+		printf("|   상점가기 (i) 기능대회(o) 보스전(p)  |\n");
 	gotoxy(70, 17);
 		printf(" ---------------------------------------\n");
+	gotoxy(37, 1);
+		printf("--GSM--");
+	gotoxy(37, 3);
+		printf("돈벌기 레벨 : %d", makegoldLV);
+	gotoxy(37, 4);
+		printf("체력 : %lld ", hp);
+	gotoxy(37, 5);
+		printf("공격력 : %lld ", pow);
 }
 
 int main() {
-	int InputKey;
 	mainscreen();
 	do {
 		InputKey = _getch();
 		if (InputKey == 'a') {
 			Admission();
+			StateUpdate();
 		}
 		if (InputKey == 's') {
 			GoldScreenUpdate();
+		}
+		if (InputKey == 'i') {
+			shop();
 		}
 	} while (InputKey != 'q');
 }
@@ -70,7 +91,7 @@ void Admission() {
 	if (gold > FirstGradePrice) {
 		gold -= 300;
 		FirstGrade++;
-		gotoxy(rand() % 17 + 5, rand() % 17 + 5);
+		gotoxy(rand() % 17 + 7, rand() % 17 + 7);
 		printf("1");
 		mainscreen();
 	}
@@ -78,10 +99,53 @@ void Admission() {
 
 //돈벌기
 void GoldScreenUpdate() {
-	gold++;
+	gold += makegold;
 	COORD pos = { 1 , 1 };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 	mainscreen();
+}
+
+//능력치
+void StateUpdate() {
+	hp = 1000;
+	pow = 100;
+	int AllOfStudent = FirstGrade + First2Grade + SecondGrade + Second2Grade + ThirdGrade;
+	hp += AllOfStudent * 5;
+	pow += AllOfStudent;
+}
+//상점 화면
+void shop() {
+	system("cls");
+	printf("내 돈 : %lld", gold);
+	gotoxy(44, 6);
+	printf("-------GSM SHOP-------\n\n");
+	gotoxy(44, 7);
+	printf("노동력 키우기(z)\n");
+	gotoxy(44, 8);
+	printf("학교 시설 강화(x)\n");
+	gotoxy(44, 9);
+	printf("기능반 전직시키기(c)\n\n");
+	gotoxy(44, 10);
+	printf("----------------------");
+}
+//상점 기능
+void shopfeature() {
+	shop();
+	do {
+		//노동력키우기
+		InputKey = _getch();
+		if (InputKey == 'z' && gold > makegoldLvPrice) {
+				goldLvup();
+				shop();
+			}
+	} while (InputKey != 'q');
+		
+}
+//돈벌기 강화 소모비용 업데이트
+void goldLvup() {
+	makegold++;
+	makegoldLvPrice += makegoldLvPrice + makegoldLV * 30;
+	gold -= makegoldLvPrice;
 }
 
 void gotoxy(int x, int y)
