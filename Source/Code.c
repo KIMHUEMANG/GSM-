0568c6aa62da575843e54298b779cbb6f2dbdae2
Kim Hue Mang 김희망 <105429536+KIMHUEMANG@users.noreply.github.com>
2022-06-05 00:11:35 +0900

    Update Code.c

--- Source/Code.c
+++ Source/Code.c
@@ -1,5 +1,13 @@
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
 //User의 Value
-long long gold = 11500;
+long long gold = 11111500;
 long long hp = 1000;
 long long pow = 100;
 int FirstGradePrice = 300;
@@ -13,7 +21,9 @@ int First2Grade = 0;
 int SecondGrade = 0;
 int Second2Grade = 0;
 int ThirdGrade = 0;
+int FeatureGrade = 0;
 int AllOfStudent = 0;
+int AllOfStudentState = 0;
 
 //Feature Function
 void gotoxy(int x, int y);
@@ -21,29 +31,32 @@ void Admission();
 void GoldScreenUpdate();
 void StateUpdate();
 void shop();
-void shopfeature();
 void goldLvup();
+void fusionFirst();
+
 
 //메인화면
 void mainscreen() {
 	gotoxy(1, 1);
 		printf("돈 : %lld 원  \n", gold);
 	gotoxy(1, 2);
-		printf("돈벌기 (s)");
+		printf("돈벌기 (spacebar)");
 	gotoxy(1, 4);
-		printf("입학받기[ - 300원 ] (a) ");
+		printf("입학받기[ - 300원 ] (a) or (s) ");
+	gotoxy(71, 1);
+		printf("총 학생 수 : %d ", AllOfStudent);
 	gotoxy(71, 3);
-		printf("총 학생 수 : %d", AllOfStudent);
+		printf("1학년 신입생 수 : %d ", FirstGrade);
 	gotoxy(71, 5);
-		printf("1학년 신입생 수 : %d", FirstGrade);
+		printf("1학년 엘리트 수 : %d ", First2Grade);
 	gotoxy(71, 7);
-		printf("1학년 엘리트 수 : %d", First2Grade);
+		printf("2학년 일반생 수 : %d ", SecondGrade);
 	gotoxy(71, 9);
-		printf("2학년 일반생 수 : %d", SecondGrade);
+		printf("2학년 엘리트 수 : %d ", Second2Grade);
 	gotoxy(71, 11);
-		printf("2학년 엘리트 수 : %d", Second2Grade);
+		printf("3학년 취준생 수 : %d ", ThirdGrade);
 	gotoxy(71, 13);
-		printf("3학년 취준생 수 : %d", ThirdGrade);
+		printf("3학년 기능반 수 : %d ", FeatureGrade);
 	gotoxy(70, 15);
 		printf(" ---------------------------------------\n");
 	gotoxy(70, 16);
@@ -58,22 +71,61 @@ void mainscreen() {
 		printf("체력 : %lld ", hp);
 	gotoxy(37, 5);
 		printf("공격력 : %lld ", pow);
+	gotoxy(6, 6);
+		printf("□□□□학교 내부□□□□");
+	gotoxy(6, 25);
+		printf("□□□□□□□□□□□□");
+	gotoxy(70, 19);
+		printf("-----------------------------------");
+		gotoxy(70, 20);
+		printf("|                                 |");
+		gotoxy(70, 21);
+		printf("| 1학년 합치기(G) L1학년 합치기(H)|");
+		gotoxy(70, 22);
+		printf("|                                 |");
+		gotoxy(70, 23);
+		printf("| 2학년 합치기(J) L2학년 합치기(K)|");
+		gotoxy(70, 24);
+		printf("|                                 |");
+		gotoxy(70, 25);
+		printf("| 3학년 합치기(L)                 |");
+		gotoxy(70, 26);
+		printf("|                                 |");
+		gotoxy(70, 27);
+		printf("-----------------------------------");
+	
+		
 }
 
 int main() {
 	mainscreen();
-	int InputKey = "";
+	char InputKey = "";
 	do {
 		InputKey = _getch();
-		if (InputKey == 'a') {
+		if (InputKey == 'a' || InputKey == 's') {
 			Admission();
 			StateUpdate();
 		}
-		if (InputKey == 's') {
+		if (InputKey == ' ') {
 			GoldScreenUpdate();
 		}
 		if (InputKey == 'i') {
 			shop();
+		}
+		if (InputKey == 'z') {
+			if (gold > makegoldLvPrice) {
+				goldLvup();
+				shop();
+			}
+		}
+		if (InputKey == 'v') {
+			system("cls");
+			mainscreen();
+		}
+		if (InputKey == 'g') {
+			fusionFirst();
+			StateUpdate();
+			mainscreen();
 		}
 	} while (InputKey != 'q');
 }
@@ -84,7 +136,7 @@ void Admission() {
 		gold -= 300;
 		FirstGrade++;
 		gotoxy(rand() % 17 + 7, rand() % 17 + 7);
-		printf("1");
+		printf("ㅇ");
 		mainscreen();
 	}
 }
@@ -101,47 +153,42 @@ void GoldScreenUpdate() {
 void StateUpdate() {
 	hp = 1000;
 	pow = 100;
-	int AllOfStudent = FirstGrade + First2Grade + SecondGrade + Second2Grade + ThirdGrade;
-	hp += AllOfStudent * 5;
-	pow += AllOfStudent;
+	AllOfStudent = FirstGrade + First2Grade + SecondGrade + Second2Grade + ThirdGrade + FeatureGrade;
+	AllOfStudentState = FirstGrade + 4 * First2Grade + 6 * SecondGrade + 10 * Second2Grade + 16 * ThirdGrade + 16 * FeatureGrade;
+	hp += AllOfStudentState * 5;
+	pow += AllOfStudentState;
 }
 //상점 화면
 void shop() {
 	system("cls");
-	printf("내 돈 : %lld", gold);
+	printf("내 돈 : %lld\n", gold);
+	printf("돈 벌기 레벨 : %d", makegoldLV);
 	gotoxy(44, 6);
-	printf("-------GSM SHOP-------\n\n");
+	printf("---------------------------GSM SHOP---------------------------\n\n");
 	gotoxy(44, 7);
-	printf("노동력 키우기(z)\n");
+	printf("              학교 노동력 키우기 [ 가격 : %lld ]  (z)\n",makegoldLvPrice);
 	gotoxy(44, 8);
-	printf("학교 시설 강화(x)\n");
+	printf("                   학교 시설 장비 구입           (x)\n");
 	gotoxy(44, 9);
-	printf("기능반 전직시키기(c)\n\n");
+	printf("                    기능반 전직시키기            (c)\n\n");
 	gotoxy(44, 10);
-	printf("----------------------");
+	printf("                      상점 나가기                (v)");
+	gotoxy(44, 11);
+	printf("--------------------------------------------------------------");
 }
 //돈벌기 강화 소모비용 업데이트
 void goldLvup() {
 	makegold++;
-	makegoldLvPrice += makegoldLvPrice + makegoldLV * 30;
+	makegoldLV++;
+	makegoldLvPrice = makegoldLvPrice + makegoldLV * 10;
 	gold -= makegoldLvPrice;
 }
-
-//상점 기능
-void shopfeature() {
-	shop();
-	int inputkey = "";
-	do {
-		inputkey = _getch();
-		//노동력키우기
-		if (inputkey == 'z' && gold > makegoldLvPrice) {
-			gotoxy(46, 11);
-			printf("노동력 레벨업!");
-				goldLvup();
-				shop();
-			}
-	} while (inputkey != 'q');
-		
+//합치기
+void fusionFirst() {
+	if (FirstGrade > 1) {
+		FirstGrade -= 2;
+		First2Grade++;
+	}
 }
 
 void gotoxy(int x, int y)
